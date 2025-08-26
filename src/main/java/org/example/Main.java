18d70a9d095e2a519c920197e0959c5d1be3c6b0
Adrian Kleinlosen <adriankleinlosen@gmail.com>
2025-08-26 12:01:12 +0200

    Fourth Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(add(3, 4, 2));
+        System.out.println(add(3, 4, 4));
         }
 
         public static int add (int a, int b, int c) {
